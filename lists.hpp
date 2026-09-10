9edc522d52fb30f1ec3c3da8c8800ffcdf6c8a2b
Baskar-Vijay <basvijay2008@gmail.com>
2026-09-10 19:40:09 +0800

    missed some win scenarios

--- lists.hpp
+++ lists.hpp
@@ -13,4 +13,13 @@ map<int, string> ResetTictactoe = {
         {8, "-"},
         {9, "-"}
 };
-vector <vector<int>> winScenarios = {{1,4,7}, {2,5,8}, {3,6,9}, {1,5,9}, {3,5,7}};
+vector <vector<int>> winScenarios = {{1,4,7}, {2,5,8}, {3,6,9}, {1,5,9}, {3,5,7},{7,8,9},{4,5,6},{1,2,3}}; // win Scenario({1,4,7}, {2,5,8}, {3,6,9}) by straight, {1,5,9}, {3,5,7}
+vector<int> winNumbers = {
+    1,4,7, 2,5,8, 3,6,9,
+    1,5,9, 3,5,7, 7,8,9,
+    4,5,6, 1,2,3
+};
+for (int i = 1; i < winNumbers.size(); i++) {
+    map <int, int > BestMove;
+    BestMove[count(winNumbers.begin(), winNumbers.end(), i)] = i;
+}
